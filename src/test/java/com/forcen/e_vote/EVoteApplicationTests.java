e24534bd87d0ce3024f5a1da7777ba7edb868a32
Babacar9792 <babacar779792@gmail.com>
2025-01-13 23:36:30 +0000

    initialisation du projet

---
+++ src/test/java/com/forcen/e_vote/EVoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forcen.e_vote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EVoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
